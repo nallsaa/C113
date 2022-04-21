3526444cffa2936c5e474a4b2a723965c3e3d7b5
nallsaa <89607954+nallsaa@users.noreply.github.com>
2022-04-21 18:46:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+tint_color = ""
+function setup(){
+canvas = createCanvas(640, 480)
+canvas.position(110, 250)
+video = createCapture(VIDEO)
+video.hide()
+}
+
+function draw(){
+    image(video, 0, 0, 640, 480)
+    tint(tint_color)
+}
+
+function filter_tint(){
+    tint_color = document.getElementById(color_name).value
+}
+
+function take_snapshot(){
+    save("img1.png")
+}
